e85ace6a7e1c556443d53f969f7a3f6efda5ca74
AshwinSreelal <ashwin.sreelal@gmail.com>
2014-10-04 07:39:27 -0700

    Added recursive implementation of the prereq tree

--- CmapsGui.py
+++ CmapsGui.py
@@ -43,23 +43,27 @@ class CmapsGui(tkinter.Tk):
             for prereq in self.coursePrereqRects:
                 for icon in prereq:
                     self.display.delete(icon) 
-            x1, y1, x2, y2 = self.centerToCorners(self.w // 4, self.h // 2, self.sSize)
+            x1, y1, x2, y2 = self.centerToCorners(self.w // 4, self.h // 2 , self.sSize)
             self.courseRect = self.display.create_rectangle(x1, y1, x2, y2, fill = "white")
             self.courseText = self.display.create_text(self.w // 4, self.h // 2, text = self.currentCourse.get_title())
             self.infoText = self.courseInfo.create_text(100, 100, anchor = "nw", 
                                                         text = "Units: " + str(self.currentCourse.get_num_units()) + "\n\n" + self.currentCourse.get_courseinfo(), 
                                                         width = self.w // 2 - 200)
-            preReqs = self.database.get_prereq_courses(self.currentCourse.get_title())
-            preReqLength = len(preReqs)
-            for i in range(preReqLength):
-                width_center = (self.w * (2 * i + 1)) / (4 * preReqLength)
-                temp = []
-                x1, y1, x2, y2 = self.centerToCorners(width_center, (self.h * 3) // 4, self.sSize)
-                temp.append(self.display.create_rectangle(x1, y1, x2, y2, fill = "white"))
-                temp.append(self.display.create_text(width_center, (self.h * 3) // 4, text = preReqs[i].get_title()))
-                temp.append(self.display.create_line(width_center, (self.h * 3) // 4 - self.sSize // 2, self.w // 4, self.h // 2 + self.sSize // 2))
-                self.coursePrereqRects.append(temp)
-
+            def printPreReqs(preReqs,level,widthleft,widthright):
+                preReqLength = len(preReqs)
+                scale=100
+                for i in range(preReqLength):
+                    width_center = widthleft+(widthright-widthleft)*(2*i+1) // (2 * preReqLength)
+                    temp = []
+                    x1, y1, x2, y2 = self.centerToCorners(width_center, self.h // 2 + scale*level, self.sSize)
+                    temp.append(self.display.create_rectangle(x1, y1, x2, y2, fill = "white"))
+                    temp.append(self.display.create_text(width_center, (self.h // 2) + scale * level , text = preReqs[i].get_title()))
+                    temp.append(self.display.create_line(width_center, (self.h // 2) + scale * level - self.sSize // 2,widthleft+ (widthright-widthleft) // 2, self.h // 2+ scale*(level-1) + self.sSize // 2))
+                    self.coursePrereqRects.append(temp)
+                    preReq2 = self.database.get_prereq_courses(preReqs[i].get_title())
+                    if(not len(preReq2) == 0):
+                        printPreReqs(preReq2,level+1,widthleft+((widthright-widthleft)*i)//len(preReqs), widthleft+((widthright-widthleft)*(i+1))//len(preReqs))
+            printPreReqs([self.currentCourse],0,0,self.w//2)
                 
         
     def centerToCorners(self, x, y, sideLength):
