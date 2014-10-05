99c49d4955ba16caae68fc2b701da0cf8e566023
AshwinSreelal <ashwin.sreelal@gmail.com>
2014-10-04 20:23:58 -0700

    Error handling has been completed

--- course.py
+++ course.py
@@ -1,5 +1,9 @@
 
 class Course:
+    """A course is an object defined by its title, course info, number of units, the classes it has a prereqs for
+    the ones that it is a prereq of and courses it is related to
+    self must be a string with no spaces, course_info must be a string with no apostrophes,num_units must be an int or float
+    prereqs,followups, related_courses, must all be lists of strings although they can be empty lists"""
     def __init__(self,title, course_info, num_units, prereqs, followups, related_courses):
         self.title = title
         self.course_info = course_info
@@ -8,29 +12,38 @@ class Course:
         self.followups = followups
         self.related_courses = related_courses
     
+    """Returns the course title """
     def get_title(self):
         return self.title
 
+    """Returns the string that is the course info """
     def get_courseinfo(self):
         return self.course_info
 
+    """ Returns an int or float, the number of units of the course"""
     def get_num_units(self):
         return self.num_units
 
+    """Returns a list of the prereqs of the course"""
     def get_prereqs(self):
         return self.prereqs
 
+    """ Returns a list of the following courses of the course """
     def get_followups(self):
         return self.followups
 
+    """Returns a list of the related courses of the course"""
     def get_related_courses(self):
         return self.related_courses
 
+    """Allows for the addition of a new prereq for the course """
     def add_prereq(self,new_prereq):
         self.prereqs.append(new_prereq)
 
+    """Allows for the addition of a new follow ups for the course """ 
     def add_followup(self,new_followup):
         self.followups.append(new_followup)
-
+    
+    """ Allows for the addition of a new related course"""
     def add_related_course(self,new_related):
         self.related_courses.append(new_related)
--- database.py
+++ database.py
@@ -1,18 +1,30 @@
 from course import Course
 class Database:
+    """ Initializes an empty dictionary for use as a database"""
     def __init__(self):
         self.courselist = {}
     
-    def add_course(self, name, course):
-        self.courselist[name] = course
-    
+    """
+    Converts a formatted file into a set of Course objects.
+    The file must be formatted in the following manner:
+
+    'course1_title' 'Description of course1' '#units' 'prereq1 prereq2' 'following1 following 2' 'related1 related2'
+    'course2_title' 'Description of course2' '#units' 'prereq1 prereq2' 'following1 following 2' 'related1 related2'
+    ...
+    Note that the last 3 fields are allowed to be empty, and that no " ' " (apostrophe) should be used in the description  
+    """ 
+
     def convert_file(self,name):
+        #Uses python's native file reader to open a formatted data file
         course_file = open(name)
+
+        #Reads the output line by line
         for line in course_file:
             read = False
             i = 0
             data = []
             text = ''
+            #The following block goes through each character in the line, alternating reading and not reading when it hits a " ' "
             for char in line:
                 if char == "'":
                     if read:
@@ -24,26 +36,39 @@ class Database:
                         read = True
                 elif read:
                     text += char
-            print(data)
+
+            #After this process, a list of strings is formed, with the lists of prereqs and the like seperated by spaces
+            #For each line the courselist adds a course, with the title of the course as the key for the dictionary
             self.courselist[data[0]] = Course(data[0], data[1], int(data[2]), data[3].split(),data[4].split(),data[5].split())
 
+    """This method enables the addition of courses into the database dictionary"""
+    def add_course(self, name, course):
+        self.courselist[name] = course
+    
+    """This method will retrieve a Course object given its name"""
     def get_course(self,name):
         return self.courselist[name]
-
+    
+    """ This method will output a list of the Course objects that are the input course's prerequisites"""
     def get_prereq_courses(self,name):
         courses = []
         for prereq in self.courselist[name].get_prereqs():
-            courses.append(self.get_course(prereq))
+            if prereq in self.courselist:
+                courses.append(self.get_course(prereq))
         return courses
 
+    """ This method will output a list of the Course objects that are the input course's classes it is a prerequisite for"""
     def get_followup_courses(self, name):
         courses = []
         for follow in self.courselist[name].get_followups():
-            courses.append(self.get_course(follow))
+            if follow in self.courselist:
+                courses.append(self.get_course(follow))
         return courses
 
+    """ This method will output a list of the Course objects that are the input course's related courses"""
     def get_related_courses(self,name):
         courses=[]
         for related in self.courselist[name].get_related_courses():
-            courses.append(self.get_course(related))
+            if related in self.courselist:
+                courses.append(self.get_course(related))
         return courses
